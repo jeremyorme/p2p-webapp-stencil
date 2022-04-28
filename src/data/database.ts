1db21a60f48412ee54c7f3bde62bd28cb893ff6f
Jeremy Orme <jeremy@heobe.com>
2022-04-28 12:53:26 +0100

    feat(*): implement store proof-of-concept

--- src/data/database.ts
+++ src/data/database.ts
@@ -1,15 +1,32 @@
 import { Business, KeyedBusiness, OtherBusiness } from './business';
+import { DbClient, Db, DbStore } from './db';
+
+// Helper to log time elapsed since previous log
+const logTime = (prev: number = null, name: string = null) => {
+  const now = performance.now();
+  if (name) console.log('PERF: ' + name + ' = ' + Math.round(now - (prev ? prev : now)) + 'ms');
+  return now;
+};
+
+// Returns a promise resolving after a number of seconds
+const waitSeconds = (seconds: number) => {
+  return new Promise((resolve, _) => {
+    setTimeout(async () => {
+      resolve(null);
+    }, 1000 * seconds);
+  });
+};
 
 class Database {
-  ipfs: any;
-  orbitdb: any;
-  myStore: any;
+  dbClient: DbClient;
+  db: Db;
+  myStore: DbStore;
   business: Business;
   businessChangedListeners: any[] = [];
   readonly: boolean;
   key: string;
-  publicStore: any;
-  businessStores: Map<string, any> = new Map();
+  publicStore: DbStore;
+  businessStores: Map<string, DbStore> = new Map();
   businesses: Map<string, KeyedBusiness[]> = new Map();
   myStoreRegistered: boolean = false;
   myStoreId: string;
@@ -20,7 +37,7 @@ class Database {
 
   async _notifyBusinessChanged() {
     // Get the latest value
-    this.business = {...this.myStore.get(this.key)};
+    this.business = this.myStore.findOne({_id: this.key});
 
     // Notify listeners
     for (const listener of this.businessChangedListeners)
@@ -29,19 +46,14 @@ class Database {
   
   async _initMyStore() {
     // Create/open a key-value store called 'business' and load its data
-    this.myStore = await this.orbitdb.keyvalue('business');
-    await this.myStore.load();
-    this.myStoreId = this.myStore.address.toString().split('/')[2];
-
-    // Get the latest value
-    this.business = {...this.myStore.get('key')};
+    this.myStore = await this.db.collection('business');
+    this.myStoreId = this.myStore.address();
 
     // Notify business changed following replication
-    this.myStore.events.on('replicated', () => this._notifyBusinessChanged());
+    this.myStore.onUpdated(() => this._notifyBusinessChanged());
 
     // Determine if the user has write access
-    const access = new Set(this.myStore.access.write);
-    this.readonly = !access.has(this.orbitdb.identity.id);
+    this.readonly = !this.myStore.canWrite();
   }
 
   async _initBusinessStore(businessStoreId: string) {
@@ -56,86 +68,93 @@ class Database {
     }
 
     // Open a key-value store called 'business' with supplied id and load its data
-    const businessStore = await this.orbitdb.keyvalue('/orbitdb/' + businessStoreId + '/business');
-    await businessStore.load();
+    const businessStore = await this.db.collection('business', {address: businessStoreId});
     this.businessStores.set(businessStoreId, businessStore);
     
     // Get all businesses from the store
-    this.businesses.set(businessStoreId, Object.entries(businessStore.all).map(e => ({key: e[0], business: e[1] as Business})));
+    this.businesses.set(businessStoreId, Array.from(businessStore.all).map(e => ({key: e[0], business: e[1] as Business})));
 
     // Update businesses for this store following replication
-    businessStore.events.on('replicated', () => this.businesses.set(businessStoreId, Object.entries(businessStore.all).map(e => ({key: e[0], business: e[1] as Business}))));
+    businessStore.onUpdated(() => {
+      this.businesses.set(businessStoreId, Array.from(businessStore.all).map(e => ({key: e[0], business: e[1] as Business})))
+    });
   }
   
   async _initBusinessStores() {
-    const businessStoreIds = this.publicStore.iterator({limit: -1}).collect().map(e => e.payload.value);
-    for (const businessStoreId of businessStoreIds)
-      await this._initBusinessStore(businessStoreId);
+    const businessStoreEntries = this.publicStore.all;
+    await Promise.all(Array.from(businessStoreEntries).map(e => this._initBusinessStore(e[1].storeId)))
   }
 
   async _initPublicStore() {
     // Create/open a public log store called 'business-store' and load its data
-    this.publicStore = await this.orbitdb.log('business-store', {accessController: {write: ['*']}});
-    await this.publicStore.load();
+    this.publicStore = await this.db.collection('business-store', {isPublic: true});
 
     // Initialize new business stores now and following replication
     await this._initBusinessStores();
-    this.publicStore.events.on('replicated', () => this._initBusinessStores());
+    this.publicStore.onUpdated(() => this._initBusinessStores());
   }
 
   async _registerMyStore() {
     // Register our own store if we haven't already done so
     if (!this.myStoreRegistered)
-      await this.publicStore.add(this.myStoreId);
+      await this.publicStore.insertOne({storeId: this.myStoreId});
   }
 
   async init() {
-    // Create IPFS instance
-    this.ipfs = window['ipfs'];
-    if (!this.ipfs) {
-      const Ipfs = window['Ipfs'];
-      this.ipfs = await Ipfs.create({
-        preload: { enabled: false },
-        EXPERIMENTAL: { pubsub: true },
-        config: {
-          Addresses: {
-            Swarm: [
-              '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-              '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-              '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
-            ]
-          },
-        }
-      });
-      window['ipfs'] = this.ipfs;
-    }
+    let t = logTime();
 
-    // Create OrbitDB instance
-    const OrbitDB = window['OrbitDB'];
-    this.orbitdb = await OrbitDB.createInstance(this.ipfs);
+    // Create db client instance
+    this.dbClient = new DbClient([
+      '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+      '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+      '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
+    ]);
+    await this.dbClient.connect();
+    t = logTime(t, 'IPFS');
+    
+    await waitSeconds(4);
+    t = logTime(t);
+
+    // Create db instance
+    this.db = await this.dbClient.db('business-directory');
+    t = logTime(t, 'DB');
     
     // Initialize stores
+    await waitSeconds(2);
+    t = logTime(t);
+
     await this._initMyStore();
+    t = logTime(t, 'My store');
+
+    await waitSeconds(1);
+    t = logTime(t);
+
     await this._initPublicStore();
+    t = logTime(t, 'Public store');
+
+    await waitSeconds(1);
+    t = logTime(t);
+
     await this._registerMyStore();
+    t = logTime(t, 'Register');
   }
 
   setBusiness(business: Partial<Business>) {
     this.business = {...this.business, ...business};
-    this.myStore.put(this.key, this.business);
+    this.myStore.insertOne({_id: this.key, ...this.business});
   }
 
   getBusiness(storeId: string, key: string): Business {
     this.key = key;
     if (this.readonly = !!storeId)
-      return this.businessStores.get(storeId).get(key);
+      return this.businessStores.get(storeId).findOne({_id: key});
     
-    this.business = this.myStore.get(this.key);
+    this.business = this.myStore.findOne({_id: this.key});
     return this.business;
   }
 
   getKeyedBusinesses(): KeyedBusiness[] {
-    return Object.entries(this.myStore.all).map(e => ({key: e[0], business: e[1] as Business}));
+    return Array.from(this.myStore.all).map(e => ({key: e[0], business: e[1] as Business}));
   }
 
   getOtherBusinesses(): OtherBusiness[] {
---
+++ src/data/db.ts
@@ -0,0 +1,372 @@
+import {read, write} from 'orbit-db-io';
+import IpfsPubsubPeerMonitor from 'ipfs-pubsub-peer-monitor';
+
+const Ipfs = window['Ipfs'];
+const OrbitDB = window['OrbitDB'];
+
+async function ipfsPut(ipfs: any, value: any): Promise<string> {
+  return write(ipfs, 'dag-pb', value);
+}
+
+async function ipfsGet<T>(ipfs: any, cid: string): Promise<T> {
+  try {
+    return (await read(ipfs, cid, {timeout: 10000}));
+  }
+  catch (_) {
+    return null;
+  }
+}
+
+function uuidv4() {
+  return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: any) =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+}
+
+function mergeArrays<T>(arraysArray: T[][]): T[] {
+  return [].concat.apply([], arraysArray);
+}
+
+function alphaPropSortFn(propName: string) {
+  return (a, b) => +(a[propName] > b[propName]) || -(a[propName] < b[propName]);
+}
+
+const byOwnerIdentity = alphaPropSortFn('ownerIdentity');
+const byClock = (a, b) => a.clock - b.clock;
+
+interface IStoreManifest {
+  name: string;
+  ownerIdentity: string;
+}
+
+interface IEntry {
+  value: any;
+  clock: number;
+}
+
+interface IEntryBlock {
+  entries: IEntry[];
+}
+
+interface IEntryBlockList {
+  ownerIdentity: string;
+  entryBlockCids: string[];
+  clock: number;
+  publicKey: string;
+  signature: string;
+}
+
+interface IStore {
+  senderIdentity: string;
+  address: string;
+  entryBlockLists: IEntryBlockList[];
+  addCount: number;
+}
+
+interface IStoreOptions {
+  address?: string;
+  isPublic?: boolean;
+  entryBlockSize?: number;
+  compactThreshold?: number;
+}
+
+const defaultStoreOptions: IStoreOptions = {
+  address: null,
+  isPublic: false,
+  entryBlockSize: 16,
+  compactThreshold: 128
+};
+
+export class DbClient {
+  private _ipfs: any;
+
+  constructor(private _swarmAddrs: string | string[] = null) {}
+
+  async connect() {
+    const swarmAddrs = !this._swarmAddrs ? [] :
+      typeof this._swarmAddrs == 'string' ? [this._swarmAddrs] : this._swarmAddrs;
+    this._ipfs = window['ipfs'];
+    if (!this._ipfs) {
+      this._ipfs = await Ipfs.create({
+        preload: { enabled: false },
+        EXPERIMENTAL: { pubsub: true },
+        config: {
+          Addresses: { Swarm: swarmAddrs },
+        }
+      });
+      window['ipfs'] = this._ipfs;
+    }
+  }
+
+  async close() {
+    await this._ipfs.stop();
+    this._ipfs = window['ipfs'] = null;
+  }
+
+  async db(name: string): Promise<Db> {
+    if (!this._ipfs)
+      return null;
+    const db = new Db(this._ipfs, name);
+    return db;
+  }
+}
+
+export class Db {
+  private _storeUpdaters: Map<string, DbStoreUpdater> = new Map();
+  private _monitor: IpfsPubsubPeerMonitor = null;
+  private _connected: boolean = false;
+  private _identity: any = null;
+
+  constructor(private _ipfs: any, private _name: string) {}
+
+  async collection(name: string, options: Partial<IStoreOptions> = {}): Promise<DbStore> {
+
+    const sub = storeJson => {
+      const store: IStore = JSON.parse(storeJson.data) as IStore;
+      if (store.senderIdentity == this._identity.id || !this._storeUpdaters.has(store.address))
+        return;
+      this._storeUpdaters.get(store.address).merge(store.entryBlockLists.sort(byOwnerIdentity));
+    };
+
+    if (!this._connected) {
+      const ipfsId = await this._ipfs.id();
+      const store = await OrbitDB.Storage(null, {}).createStore('./orbitdb/' + ipfsId.id + '/keystore');
+      const keystore = new OrbitDB.Keystore(store);
+      this._identity = await OrbitDB.Identities.createIdentity({ id: ipfsId.id, keystore });
+
+      await this._ipfs.pubsub.subscribe('/db/' + this._name, sub);
+      this._monitor = new IpfsPubsubPeerMonitor(this._ipfs.pubsub, '/db/' + this._name)
+      this._monitor.on('join', peer => {
+        for (const storeUpdater of this._storeUpdaters.values())
+          storeUpdater.onPeerJoined(peer);
+      });
+      this._connected = true;
+    }
+
+    const pub = (store: IStore) => {
+      const storeJson = JSON.stringify(store)
+      this._ipfs.pubsub.publish('/db/' + this._name, storeJson);
+    };
+
+    const storeUpdater = new DbStoreUpdater(this._ipfs, this._identity, pub);
+    await storeUpdater.init(this._name + '/' + name, options);
+    this._storeUpdaters.set(storeUpdater.address(), storeUpdater);
+
+    return new DbStore(storeUpdater);
+  }
+}
+
+class DbStoreUpdater {
+  private _options: IStoreOptions;
+  private _address: string;
+  private _ownerIdentity: string;
+  private _index: Map<string, any> = new Map();
+  private _clock: number = 0;
+  private _entryBlockLists: Map<string, IEntryBlockList> = new Map();
+  private _storeCid: string;
+  private _updatedCallbacks: Array<() => void> = [];
+  private _addCount: number = 0;
+  private _numEntries: number = 0;
+
+  constructor(
+    private _ipfs: any,
+    private _identity: any,
+    private _publish: (IStore) => void) {}
+
+  async init(name: string, options: Partial<IStoreOptions>) {
+    this._options = {...defaultStoreOptions, ...options};
+
+    let manifest: IStoreManifest;
+    if (options.address) {
+      this._address = options.address;
+      manifest = await ipfsGet<IStoreManifest>(this._ipfs, this._address);
+      if (!manifest) {
+        console.log('Store address ' + this._address + ' could not be reached');
+        return;
+      }
+      this._ownerIdentity = manifest.ownerIdentity;
+    }
+    else {
+      this._ownerIdentity = options.isPublic ? '*' : this._identity.id;
+      manifest = {name, ownerIdentity: this._ownerIdentity};
+      this._address = await ipfsPut(this._ipfs, manifest);
+    }
+
+    const storeCid = window.localStorage.getItem('/db/' + this._address);
+    if (storeCid) {
+      const store: IStore = storeCid ? await ipfsGet<IStore>(this._ipfs, storeCid) : null;
+      this._addCount = store.addCount;
+      await this.merge(store.entryBlockLists);
+    }
+  }
+
+  async merge(entryBlockLists: IEntryBlockList[]) {
+    entryBlockLists.filter(entryBlockList => this._canMerge(entryBlockList)).forEach(entryBlockList => {
+      this._entryBlockLists.set(entryBlockList.ownerIdentity, entryBlockList);
+    });
+    if (!await this._updateStoreCid())
+      return;
+
+    const entryBlockCids: string[] = mergeArrays(Array.from(this._entryBlockLists.values()).sort(byOwnerIdentity).map(ebl => ebl.entryBlockCids));
+    const entryBlocks: IEntryBlock[] = await Promise.all(entryBlockCids.map(entryBlockCid => ipfsGet<IEntryBlock>(this._ipfs, entryBlockCid)));
+    const allEntries: IEntry[] = mergeArrays(entryBlocks.map(eb => eb ? eb.entries : []));
+    this._numEntries = allEntries.length;
+
+    allEntries.sort(byClock);
+    if (allEntries.length > 0)
+      this._clock = allEntries.slice(-1)[0].clock;
+
+    this._index.clear();
+    for (const entry of allEntries)
+      this._index.set(entry.value._id, entry.value);
+  }
+
+  _canMerge(entryBlockList: IEntryBlockList) {
+    if (this._entryBlockLists.has(entryBlockList.ownerIdentity) && entryBlockList.clock <= this._entryBlockLists.get(entryBlockList.ownerIdentity).clock ||
+      this._ownerIdentity != '*' && this._ownerIdentity != entryBlockList.ownerIdentity ||
+      !entryBlockList.signature ||
+      !entryBlockList.publicKey)
+      return false;
+
+    const e = {...entryBlockList, signature: ''};
+    return this._identity.provider.verify(entryBlockList.signature, entryBlockList.publicKey, JSON.stringify(e), 'v1');
+  }
+
+  async _updateStoreCid() {
+    const store: IStore = {
+      senderIdentity: this._identity.id,
+      address: this._address,
+      entryBlockLists: Array.from(this._entryBlockLists.values()),
+      addCount: this._addCount }; 
+
+    const newStoreCid = await ipfsPut(this._ipfs, store);
+
+    if (newStoreCid == this._storeCid)
+      return false;
+
+    this._storeCid = newStoreCid;
+    window.localStorage.setItem('/db/' + this._address, this._storeCid);
+    this._publish(store);
+    for (const cb of this._updatedCallbacks)
+      cb();
+
+    return true;
+  }
+
+  async add(objs: any[]) {
+    for (const obj of objs)
+      this._index.set(obj._id, obj);
+    this._numEntries += objs.length;
+
+    if (!this._entryBlockLists.has(this._identity.id))
+      this._entryBlockLists.set(this._identity.id, {
+        ownerIdentity: this._identity.id,
+        entryBlockCids: [],
+        clock: null,
+        publicKey: this._identity.publicKey,
+        signature: null
+      });
+
+    const myEntryBlockList: IEntryBlockList = this._entryBlockLists.get(this._identity.id);
+    const lastBlock: IEntryBlock = myEntryBlockList.entryBlockCids.length > 0 ?
+      await ipfsGet<IEntryBlock>(this._ipfs, myEntryBlockList.entryBlockCids.slice(-1)[0]) : {entries: []};
+
+    const lastEntries = lastBlock.entries.length != this._options.entryBlockSize ? lastBlock.entries : [];
+    let newBlockEntries = [...lastEntries, ...objs.map(obj => ({value: obj, clock: ++this._clock}))];
+
+    const newBlocks = [];
+    while (newBlockEntries.length > 0) {
+      newBlocks.push({entries: newBlockEntries.slice(0, this._options.entryBlockSize)});
+      newBlockEntries = newBlockEntries.slice(this._options.entryBlockSize);
+    }
+
+    const newBlockCids = await Promise.all(newBlocks.map(eb => ipfsPut(this._ipfs, eb)));
+    const oldBlockCids = lastEntries.length > 0 ?
+      myEntryBlockList.entryBlockCids.slice(0, myEntryBlockList.entryBlockCids.length - 1) :
+      myEntryBlockList.entryBlockCids;
+
+    myEntryBlockList.entryBlockCids = [...oldBlockCids, ...newBlockCids];
+
+    this._addCount += objs.length;
+    if (this._addCount >= this._options.compactThreshold) {
+      this._addCount %= this._options.compactThreshold;
+      await this._compact();
+    }
+
+    myEntryBlockList.clock = this._clock;
+    myEntryBlockList.signature = '';
+    myEntryBlockList.signature = await this._identity.provider.sign(this._identity, JSON.stringify(myEntryBlockList));
+
+    await this._updateStoreCid();
+  }
+
+  async _compact() {
+    const myEntryBlockList: IEntryBlockList = this._entryBlockLists.get(this._identity.id);
+    let myEntryBlocks: IEntryBlock[] = await Promise.all(myEntryBlockList.entryBlockCids.map(entryBlockCid => ipfsGet<IEntryBlock>(this._ipfs, entryBlockCid)));
+    const myEntries: IEntry[] = mergeArrays(myEntryBlocks.map(eb => eb.entries));
+
+    const myEffectiveEntryMap: Map<string, IEntry> = new Map();
+    for (const entry of myEntries)
+      myEffectiveEntryMap.set(entry.value._id, entry);
+    const myEffectiveEntries = Array.from(myEffectiveEntryMap.values());
+    this._numEntries += myEffectiveEntries.length - myEntries.length;
+
+    myEntryBlocks = [];
+    let sortedEntries = myEffectiveEntries.sort(byClock);
+    while (sortedEntries.length > 0) {
+      myEntryBlocks.push({entries: sortedEntries.slice(0, this._options.entryBlockSize)});
+      sortedEntries = sortedEntries.slice(this._options.entryBlockSize);
+    }
+
+    myEntryBlockList.entryBlockCids = await Promise.all(myEntryBlocks.map(eb => ipfsPut(this._ipfs, eb)));
+  }
+
+  async onPeerJoined(_peer: string) {
+    if (this._entryBlockLists.size > 0)
+      this._publish({
+        senderIdentity: this._identity.id,
+        address: this._address,
+        entryBlockLists: Array.from(this._entryBlockLists.values()),
+        addCount: this._addCount
+      });
+  };
+
+  onUpdated(callback: () => void) { this._updatedCallbacks.push(callback); }
+  
+  canWrite(): boolean { return this._identity == this._ownerIdentity || this._ownerIdentity == '*'; }
+
+  address(): string { return this._address; }
+
+  index(): Map<string, any> { return this._index; }
+
+  numEntries(): number { return this._numEntries; }
+}
+
+export class DbStore {
+  constructor(private _updater: DbStoreUpdater) {}
+
+  async insertOne(doc: any) {
+    const docWithId = doc._id ? doc : {...doc, _id: uuidv4()};
+    await this._updater.add([docWithId]);
+    return docWithId._id;
+  }
+
+  async insertMany(docs: any[]) {
+    const docsWithId = docs.map(doc => doc._id ? doc : {...doc, _id: uuidv4()});
+    await this._updater.add(docsWithId);
+    return docsWithId.map(doc => doc._id);
+  }
+
+  findOne(query: any) {
+    return query._id && Object.keys(query).length == 1 ? this._updater.index().get(query._id) : null;
+  }
+
+  public get all() { return this._updater.index().entries(); }
+
+  onUpdated(callback: () => void) { this._updater.onUpdated(callback); }
+
+  canWrite() { return this._updater.canWrite(); }
+
+  address() { return this._updater.address(); }
+
+  numEntries(): number { return this._updater.numEntries(); }
+}
